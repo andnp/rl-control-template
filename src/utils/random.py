5a682dd1af8187efbeb71ab93f5cc1675059897c
yasuiniko <nikolaus.yasui@mail.mcgill.ca>
2020-04-13 15:33:55 -0600

    fixed bug where max value was added to array instead of the index of max value

--- src/utils/random.py
+++ src/utils/random.py
@@ -23,7 +23,7 @@ def choice(arr):
 # argmax that breaks ties randomly
 def argmax(vals):
     top = vals[0]
-    ties = [top]
+    ties = [0]
     for i, v in enumerate(vals):
         if v > top:
             top = v
@@ -65,7 +65,7 @@ def uniform_argchoice_rng(rng, arr):
 # argmax that breaks ties randomly
 def argmax_rng(rng, vals):
     top = vals[0]
-    ties = [top]
+    ties = [0]
     for i, v in enumerate(vals):
         if v > top:
             top = v
