c48400c1924698174f1daaf6d31c0d2c248b8514
andy <andnpatterson@gmail.com>
2020-04-30 10:38:46 -0600

    migrate to PyExpUtils@1.5

--- src/utils/arrays.py
+++ src/utils/arrays.py
@@ -1,25 +1,3 @@
-from itertools import tee, filterfalse
-
-def fillRest(arr, val, length):
-    for i in range(len(arr), length):
-        arr.append(val)
-
-    return arr
-
-def first(listOrGen):
-    if type(listOrGen) == list:
-        return listOrGen[0]
-
-    return next(listOrGen)
-
-def last(l):
-    return l[len(l) - 1]
-
-def partition(gen, pred):
-    t1, t2 = tee(gen)
-
-    return filter(pred, t2), filterfalse(pred, t1)
-
 def isIterable(thing):
     try:
         _ = (x for x in thing)
