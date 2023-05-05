2d4e5e1f12ee3ea1cef5fdb965fb48b9e28d194e
andy <andnpatterson@gmail.com>
2023-05-05 12:52:26 -0600

    fix: make sure minatar envs are pickleable

--- src/problems/Minatar.py
+++ src/problems/Minatar.py
@@ -1,23 +1,22 @@
-from experiment.ExperimentModel import ExperimentModel
-from problems.BaseProblem import BaseProblem
+from functools import partial
 from environments.Minatar import Minatar
+from problems.BaseProblem import BaseProblem
 from PyExpUtils.utils.Collector import Collector
+from experiment.ExperimentModel import ExperimentModel
 
-def build_minatar_problem(game: str):
-    class MinatarProblem(BaseProblem):
-        def __init__(self, exp: ExperimentModel, idx: int, collector: Collector):
-            super().__init__(exp, idx, collector)
+class MinatarProblem(BaseProblem):
+    def __init__(self, exp: ExperimentModel, idx: int, collector: Collector, game: str):
+        super().__init__(exp, idx, collector)
 
-            self.env = Minatar(game, self.seed)
-            self.actions = self.env.env.num_actions()
+        self.env = Minatar(game, self.seed)
+        self.actions = self.env.env.num_actions()
 
-            self.observations = self.env.env.state_shape()
-            self.gamma = 0.99
+        self.observations = self.env.env.state_shape()
+        self.gamma = 0.99
 
-    return MinatarProblem
 
-Breakout = build_minatar_problem('breakout')
-Seaquest = build_minatar_problem('seaquest')
-Asterix = build_minatar_problem('asterix')
-Freeway = build_minatar_problem('freeway')
-SpaceInvaders = build_minatar_problem('space_invaders')
+Breakout = partial(MinatarProblem, game='breakout')
+Seaquest = partial(MinatarProblem, game='seaquest')
+Asterix = partial(MinatarProblem, game='asterix')
+Freeway = partial(MinatarProblem, game='freeway')
+SpaceInvaders = partial(MinatarProblem, game='space_invaders')
