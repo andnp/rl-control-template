30dbfb0cea9d32505e5f4f87b90bae643bfe17de
andy <andnpatterson@gmail.com>
2023-08-29 20:40:09 -0600

    add some minor logging

--- scripts/setup_cc.sh
+++ scripts/setup_cc.sh
@@ -4,10 +4,13 @@ module load python/3.11
 
 # make sure home folder has a venv
 if [ ! -d "~/.venv" ]; then
+  echo "making a new virtual env in ~/.venv"
   python -m venv ~/.venv
 fi
 
-source ~/.venv
+source ~/.venv/bin/activate
+echo "installing PyExpUtils"
 pip install PyExpUtils-andnp
 
+echo "scheduling a job to install project dependencies"
 sbatch salloc --time=0:15:0 --ntasks=1 --mem-per-cpu="4G" --account=rrg-whitem --export=path="$(pwd)" scripts/local_node_venv.sh
