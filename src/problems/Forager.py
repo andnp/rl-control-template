3313a0afe2fd4b05e78d497582caf6559c0e6c55
Niko Yasui <yasuiniko@gmail.com>
2024-10-14 15:43:20 -0600

    fix(Forager): added code to get observation dims from environment directly
    Previously, incorrect values were hard-coded into problems/Forager.py. Now they are dynamically generated.
    
    Fixes: #16

--- src/problems/Forager.py
+++ src/problems/Forager.py
@@ -10,5 +10,13 @@ class Forager(BaseProblem):
         self.env = Env(self.seed)
         self.actions = 4
 
-        self.observations = (9, 9, 4)
+        # get aperture size from env
+        # should be initialized when Env() is called
+        assert self.env.env._ap_size is not None
+        ap_x, ap_y = self.env.env._ap_size
+
+        # get number of object types from env
+        num_objects = len(self.env.env._names)
+
+        self.observations = (ap_x, ap_y, num_objects)
         self.gamma = 0.99
